cc0302340f98a5d71a0c05b9000ee36448dd012c
omerhac <omerhac94@gmail.com>
2023-11-16 23:37:40 +0200

    setup: added setup.py for pip installation

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='pixel_brain',
+    version='0.1',
+    packages=find_packages(exclude=["tests", "tests/*"]),
+    description='A package for procssing image data using ML models',
+    author='Omer Hacohen',
+    author_email='omerhac94@gmail.com',
+    url='https://github.com/omerhac/pixel-brain.git',  # URL of your package's source code
+    install_requires=required,
+    entry_points={
+        'apps': [
+            'tag_identity = pixel_brain.apps.tag_identity:main',
+        ],
+    }
+)
