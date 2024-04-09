c3ee5b1dbae593c7f78ca82abab93709ecd58b08
Nir Weingarten <nirzvi89@gmail.com>
2024-04-09 17:46:11 +0300

    Added features and changes to face_extractor:
    1. Bounding boxes are computed as a ratio of original box
    2. Multiple faces can be extracted from one image
    3. Doesn't raise exception if no face was detected, instead skips the
       image
    4. Masks other faces that are not the extracted face

--- src/pixelbrain/utils.py
+++ src/pixelbrain/utils.py
@@ -2,6 +2,7 @@ import os
 import logging
 import sys
 import time
+from typing import Tuple
 
 
 PIXELBRAIN_PATH = os.getenv("PIXELBRAIN_PATH", None)
@@ -20,3 +21,20 @@ def get_logger(log_name):
 
 def create_timestamp():
     return time.strftime("%Y%m%d-%H%M%S")
+
+
+def increase_rectangle_space(x: float, y: float, w: float, h: float, ratio: float = 1.5) -> Tuple[int, int, int, int]:
+    """
+    Increases a rectangle's space by a given ratio.
+    :param x: x coordinate of the rectangle
+    :param y: y coordinate of the rectangle
+    :param w: width of the rectangle
+    :param h: height of the rectangle
+    :param ratio: the ratio to increase the rectangle space by
+    :return: new x, new y, new w, new h
+    """
+    new_w = w * ratio
+    new_h = h * ratio
+    new_x = max(0, x - (new_w - w) * 0.5)
+    new_y = max(0, y - (new_h - h) * 0.5)
+    return int(new_x), int(new_y), int(new_w), int(new_h)
