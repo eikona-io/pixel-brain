206adee64e46482bb956dbe9cfc0ae8b156eff2c
omer <omer@eikona.io>
2024-04-28 12:59:46 +0300

    requirements: changed to 3.10

--- src/tests/test_face_extractor.py
+++ src/tests/test_face_extractor.py
@@ -5,29 +5,29 @@ import tempfile
 import os 
 from pixelbrain.utils import PIXELBRAIN_PATH
 import pytest
+from tests.test_utils import DeleteDatabaseAfterTest
+from uuid import uuid4
 
 
 @pytest.mark.slow_suit
 def test_face_extractor_module():
-    database = Database(database_id="face_extractor")
-    data = DataLoader(f"{PIXELBRAIN_PATH}/assets/test_data/", database)
-    
-    with tempfile.TemporaryDirectory() as tempdir:
-        module = FaceExtractorModule(data, database, image_save_path=tempdir)
-        module.process()
-        num_images = len(os.listdir(tempdir))
-        assert num_images == 31 + 2 * 3, "there should be 31 faces from portrait images and 2 * 3 faces from two triplete images"
-    
-    database.delete_db()
+    database = Database(database_id=uuid4().hex)
+    with DeleteDatabaseAfterTest(database):
+        data = DataLoader(f"{PIXELBRAIN_PATH}/assets/test_data/", database)
+        
+        with tempfile.TemporaryDirectory() as tempdir:
+            module = FaceExtractorModule(data, database, image_save_path=tempdir)
+            module.process()
+            num_images = len(os.listdir(tempdir))
+            assert num_images == 31 + 2 * 3, "there should be 31 faces from portrait images and 2 * 3 faces from two triplete images"
 
 def test_face_extractor_module_subset():
-    database = Database(database_id="face_extractor")
-    data = DataLoader(f"{PIXELBRAIN_PATH}/assets/test_data/subset", database)
-    
-    with tempfile.TemporaryDirectory() as tempdir:
-        module = FaceExtractorModule(data, database, image_save_path=tempdir)
-        module.process()
-        num_images = len(os.listdir(tempdir))
-        assert num_images == 1, "there should be 1 faces"
-    
-    database.delete_db()
+    database = Database(database_id=uuid4().hex)
+    with DeleteDatabaseAfterTest(database):
+        data = DataLoader(f"{PIXELBRAIN_PATH}/assets/test_data/subset", database)
+        
+        with tempfile.TemporaryDirectory() as tempdir:
+            module = FaceExtractorModule(data, database, image_save_path=tempdir)
+            module.process()
+            num_images = len(os.listdir(tempdir))
+            assert num_images == 1, "there should be 1 faces"
