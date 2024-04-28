1173dec404a7a7c3dc1ad2ebb5eb17070ce8aa74
omer <omer@eikona.io>
2024-04-28 12:22:53 +0300

    test-resnet-classifier: use unique names and delete db after

--- src/tests/test_resnet_classifier.py
+++ src/tests/test_resnet_classifier.py
@@ -2,44 +2,47 @@ from pixelbrain.modules.resnet_classifier import ResnetClassifierModule
 from pixelbrain.data_loader import DataLoader
 from pixelbrain.database import Database
 from pixelbrain.utils import PIXELBRAIN_PATH
+from tests.test_utils import DeleteDatabaseAfterTest
+from uuid import uuid4
 
 
 def test_resnet_classifier_module():
     # Create a dummy database
-    database = Database(database_id='test_db')
+    database_id = uuid4().hex
+    database = Database(database_id=database_id)
+    with DeleteDatabaseAfterTest(database):
+        # Load images from assets/test_images
+        images_path = f"{PIXELBRAIN_PATH}/assets/test_data"
+        data_loader = DataLoader(images_path, database, batch_size=2)
 
-    # Load images from assets/test_images
-    images_path = f'{PIXELBRAIN_PATH}/assets/test_data'
-    data_loader = DataLoader(images_path, database, batch_size=2)
+        # Initialize ResnetClassifierModule
+        resnet_classifier = ResnetClassifierModule(data_loader, database)
 
-    # Initialize ResnetClassifierModule
-    resnet_classifier = ResnetClassifierModule(data_loader, database)
+        resnet_classifier.process()
 
-    resnet_classifier.process()
-
-    processed_metadata = database.get_all_images()
-    for image_meta in processed_metadata:
-        assert 'class' in image_meta
-    # Delete the dummy database after the test
-    database.delete_db()
+        processed_metadata = database.get_all_images()
+        for image_meta in processed_metadata:
+            assert "class" in image_meta
 
 
 def test_resnet_classifier_module_2_classes():
     # Create a dummy database
-    database = Database(database_id='test_db')
+    database_id = uuid4().hex
+    database = Database(database_id=database_id)
+    with DeleteDatabaseAfterTest(database):
 
-    # Load images from assets/test_images
-    images_path = f'{PIXELBRAIN_PATH}/assets/test_data'
-    data_loader = DataLoader(images_path, database, batch_size=2)
+        # Load images from assets/test_images
+        images_path = f"{PIXELBRAIN_PATH}/assets/test_data"
+        data_loader = DataLoader(images_path, database, batch_size=2)
 
-    # Initialize ResnetClassifierModule
-    resnet_classifier = ResnetClassifierModule(data_loader, database, class_subset=['sweatshirt', 'tiger cat'])
+        # Initialize ResnetClassifierModule
+        resnet_classifier = ResnetClassifierModule(
+            data_loader, database, class_subset=["sweatshirt", "tiger cat"]
+        )
 
-    resnet_classifier.process()
+        resnet_classifier.process()
 
-    processed_metadata = database.get_all_images()
-    for image_meta in processed_metadata:
-        assert 'class' in image_meta
-        assert image_meta['class'] == 'sweatshirt'
-    # Delete the dummy database after the test
-    database.delete_db()
+        processed_metadata = database.get_all_images()
+        for image_meta in processed_metadata:
+            assert "class" in image_meta
+            assert image_meta["class"] == "sweatshirt"
