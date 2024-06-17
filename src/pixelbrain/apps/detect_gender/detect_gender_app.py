309441fa3e89fd53da8c5525da08c9460de22fc3
omer <omer@eikona.io>
2024-06-17 09:54:47 +0300

    gender-detection-app: refactored to be able to use s3 as well

--- src/pixelbrain/apps/detect_gender/detect_gender_app.py
+++ src/pixelbrain/apps/detect_gender/detect_gender_app.py
@@ -3,50 +3,83 @@ import torch
 from os.path import join
 from os import environ
 from pixelbrain.data_loaders.cloudinary_dataloader import CloudinaryDataLoader
+from pixelbrain.data_loaders.s3_dataloader import S3DataLoader
 from pixelbrain.database import Database
 from uuid import uuid4
-
+from typing import Literal, Optional
 
 MAX_BATCH_SIZE = 32
 
 
-class CloudinaryGenderDetector:
+class GenderDetectorApp:
     """
-    A class to detect gender using images stored in Cloudinary.
+    A class to detect gender using images stored in Cloudinary or S3.
     For now we only consider the binary case.
     """
 
-    def __init__(self, cloudinary_prefix: str, num_images: int = 10, download_from_hf: bool = False, model_name: str = 'gender-classification'):
+    def __init__(
+        self,
+        remote_prefix: str,
+        num_images: int = 10,
+        download_from_hf: bool = False,
+        model_name: str = "gender-classification",
+        source_type: Literal["cloudinary", "s3"] = "s3",
+        bucket_name: Optional[str] = None,
+    ):
         """
         Uses a HF model to detect the gender of a user based on their images as stored in the cloudinary processed folder.
         :param user_id: The user id to process
         :param num_images: Takes the average score of the first <num_images> images
         :param download_from_hf: If True, downloads the model from Hugging Face
         :param model_name: The name of the model to use. If from Hugging Face, it should be the full model name in the HF hub, else the name of the direcotry where the model is stored under $HOME/
+        :param source_type: The source type to use. Either 'cloudinary' or 's3'
+        :param bucket_name: The name of the bucket to use if source_type is 's3'
         """
-        self.cloudinary_prefix = cloudinary_prefix
+        self.remote_prefix = remote_prefix
         self.num_images = num_images
         self.download_from_hf = download_from_hf
         self.model_name = model_name
+        self.source_type = source_type
+        self.bucket_name = bucket_name
 
     def process(self) -> float:
         """Processes the images to detect gender. Returns probability of being a female"""
-        if self.download_from_hf:            
-            processor = AutoImageProcessor.from_pretrained("rizvandwiki/gender-classification")
-            model = AutoModelForImageClassification.from_pretrained("rizvandwiki/gender-classification")
+        if self.download_from_hf:
+            processor = AutoImageProcessor.from_pretrained(
+                "rizvandwiki/gender-classification"
+            )
+            model = AutoModelForImageClassification.from_pretrained(
+                "rizvandwiki/gender-classification"
+            )
         else:
-            local_model_path = join(environ.get('HOME'), self.model_name.split('/')[-1])
+            local_model_path = join(environ.get("HOME"), self.model_name.split("/")[-1])
             processor = AutoImageProcessor.from_pretrained(local_model_path)
             model = AutoModelForImageClassification.from_pretrained(local_model_path)
             model.eval()
 
         local_temp_database = Database(database_id=uuid4().hex)
 
-        dataloader = CloudinaryDataLoader(self.cloudinary_prefix, local_temp_database, min(MAX_BATCH_SIZE, self.num_images))
+        if self.source_type == "cloudinary":
+            dataloader = CloudinaryDataLoader(
+                self.remote_prefix,
+                local_temp_database,
+                min(MAX_BATCH_SIZE, self.num_images),
+            )
+        elif self.source_type == "s3":
+            dataloader = S3DataLoader(
+                self.remote_prefix,
+                self.bucket_name,
+                local_temp_database,
+            )
+        else:
+            raise ValueError(f"Invalid source type: {self.source_type}")
+
         if not len(dataloader):
             import os
-            raise RuntimeError(f"There are no images in {self.cloudinary_prefix}, defined cloudinary url: {os.getenv('CLOUDINARY_URL')}")
 
+            raise RuntimeError(
+                f"There are no images in {self.remote_prefix}, defined cloudinary url: {os.getenv('CLOUDINARY_URL')}"
+            )
 
         max_iterations = max(1, self.num_images // dataloader._batch_size)
         results_tensor_list = []
@@ -57,5 +90,7 @@ class CloudinaryGenderDetector:
             results_tensor_list.append(probs)
             if i == max_iterations:
                 break
-        probability_to_be_female = torch.cat(results_tensor_list, dim=0).mean(dim=0)[0].item()
+        probability_to_be_female = (
+            torch.cat(results_tensor_list, dim=0).mean(dim=0)[0].item()
+        )
         return probability_to_be_female
--- src/tests/detect_gender/test_detect_gender_app.py
+++ src/tests/detect_gender/test_detect_gender_app.py
@@ -1,4 +1,4 @@
-from pixelbrain.apps.detect_gender.cloudinary_detect_gender_app import CloudinaryGenderDetector
+from pixelbrain.apps.detect_gender.detect_gender_app import GenderDetectorApp
 from tests.test_utils import assert_env_var_present
 
 
@@ -6,6 +6,23 @@ def test_cloudinary_gender_detector_app():
     assert assert_env_var_present("CLOUDINARY_URL"), "CLOUDINARY_URL is not set"
 
     test_user_cloudinary_prefix = "user_photos/1702595526432"
-    app = CloudinaryGenderDetector(test_user_cloudinary_prefix, download_from_hf=True)
+    app = GenderDetectorApp(test_user_cloudinary_prefix, download_from_hf=True)
     prob_for_famale = app.process()
-    assert prob_for_famale > 0.5, "Probability for female is less then 0.5 (subject IS a female)"
+    assert (
+        prob_for_famale > 0.5
+    ), "Probability for female is less then 0.5 (subject IS a female)"
+
+
+def test_s3_gender_detector_app():
+    bucket_name = "images.selfyx.com"
+    test_user_prefix = "user_photos/1702595526432"
+    app = GenderDetectorApp(
+        test_user_prefix,
+        download_from_hf=True,
+        source_type="s3",
+        bucket_name=bucket_name,
+    )
+    prob_for_famale = app.process()
+    assert (
+        prob_for_famale > 0.5
+    ), "Probability for female is less then 0.5 (subject IS a female)"
